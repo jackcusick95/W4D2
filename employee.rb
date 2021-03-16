76372a65bb38be646013ba317eefccc028ece33f
Abir Al Mahamud <abirmahamud610@gmail.com>
2021-03-16 13:48:59 -0400

    Add employee and manager class

---
+++ employee.rb
@@ -0,0 +1,17 @@
+class Employee
+
+    def initialize(name, title, salary, boss)
+        @name = name 
+        @title = title 
+        @salary = salary 
+        @boss = boss
+    end
+
+    def bonus(multiplier)
+        @salary * multiplier
+    end
+
+    
+
+end
+
---
+++ manager.rb
@@ -0,0 +1,11 @@
+require_relative 'employee.rb'
+
+class Manager < Employee
+
+    def initialize
+        @employees = []
+    end
+
+    
+
+end
