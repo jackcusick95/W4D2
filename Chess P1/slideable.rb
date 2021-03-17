2ff8ef7de072f93f50558d9e6e008029d8a96f79
Abir Al Mahamud <abirmahamud610@gmail.com>
2021-03-17 13:56:48 -0400

    Add slideable and stepable Modules

---
+++ Chess P1/slideable.rb
@@ -0,0 +1,41 @@
+module Slideable
+
+    HORIZONTAL_DIRS = [
+        [0, -1]
+        [0, 1]
+        [1, 0]
+        [-1, 0]
+    ]
+
+    DIAGONAL_DIRS = [
+        [1, -1]
+        [1 ,1]
+        [-1, -1]
+        [-1, 1]
+    ]
+
+    def horizontal_dirs
+        self.
+    end
+
+    def diagonal_dirs
+
+    end
+
+    def moves
+
+    end
+
+
+    private
+
+    def move_dirs
+
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+
+    end
+
+
+end
